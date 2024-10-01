083b66693919828a01c9cdfc0b967561b4ab2a2d
Jussi Maki <jussi.maki@isovalent.com>
2024-10-01 14:07:53 +0200

    script: Vendor in rsc.io/script
    Vendor in the rsc.io/script library. As it was essentially
    a code dump out from go's internals and is unmaintained by
    rsc, it's easiest to vendor it here.
    
    It is vendored into cilium/hive instead of its own repo as
    its use and extensions will be somewhat coupled with hive.
    
    As first extension, add support for interactive use that
    doesn't wait for the section end.
    
    Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

---
+++ script/scripttest/scripttest_test.go
@@ -0,0 +1,25 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package scripttest_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/cilium/hive/script"
+	"github.com/cilium/hive/script/scripttest"
+)
+
+func TestAll(t *testing.T) {
+	ctx := context.Background()
+	engine := &script.Engine{
+		Conds: scripttest.DefaultConds(),
+		Cmds:  scripttest.DefaultCmds(),
+		Quiet: !testing.Verbose(),
+	}
+	env := os.Environ()
+	scripttest.Test(t, ctx, func() *script.Engine { return engine }, env, "testdata/*.txt")
+}
