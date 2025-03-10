cilium/hive

commit 08596a874b9238a85e3548415e4370209c564b33

script: Fix lack of newline on retried command in logs
and show the amount of time it took until retries succeeded.

Before:

    scripttest.go:72: # Test command retrying and the flag parsing when retrying.
        # The 'retrytest' command should run two times and the flags
        # should not be lost.> * retrytest --not-empty=xyz abc
    scripttest.go:72:  (0.010s)
        (command "* retrytest --not-empty=xyz abc" failed, retrying in 10ms...)
        (command "* retrytest --not-empty=xyz abc" succeeded after 1 retries)
    --- PASS: TestAll/retry (0.01s)

After:

    scripttest.go:72: # Test command retrying and the flag parsing when retrying.
        # The 'retrytest' command should run two times and the flags
        # should not be lost.
        > * retrytest --not-empty=xyz abc
        retrytest called
    scripttest.go:72: (command "* retrytest --not-empty=xyz abc" failed, retrying in 10ms...)
        retrytest called
    scripttest.go:72: (command "* retrytest --not-empty=xyz abc" succeeded after 1 retries in 0.010s)
    --- PASS: TestAll/retry (0.01s)

Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

script/engine.go
@@ -341,12 +341,20 @@ func (e *Engine) Execute(s *State, file string, script *bufio.Reader, log io.Wri
 		err = e.runCommand(s, cmd, impl)
 		if err != nil {
 			if cmd.want == successRetryOnFailure || cmd.want == failureRetryOnSuccess {
+				retryStart := sectionStart
+
+				// Clear the section start to avoid the deferred endSection() printing a timestamp.
+				sectionStart = time.Time{}
+
+				// Append the new line that section start omitted and flush the log.
+				io.WriteString(log, "\n")
+				s.FlushLog()
+
 				// Command wants retries. Retry the whole section
 				backoff := exponentialBackoff{max: maxRetryInterval, interval: retryInterval}
 				for err != nil {
-					s.FlushLog()
 					retryDuration := backoff.get()
-					s.Logf("(command %q failed, retrying in %s...)\n", line, retryDuration)
+					fmt.Fprintf(log, "(command %q failed, retrying in %s...)\n", line, retryDuration)
 					select {
 					case <-s.Context().Done():
 						s.RetryCount = 0
@@ -360,9 +368,11 @@ func (e *Engine) Execute(s *State, file string, script *bufio.Reader, log io.Wri
 							break
 						}
 					}
+					s.FlushLog()
 				}
-				s.Logf("(command %q succeeded after %d retries)\n", line, s.RetryCount)
+				fmt.Fprintf(log, "(command %q succeeded after %d retries in %.3fs)\n", line, s.RetryCount, time.Since(retryStart).Seconds())
 				s.RetryCount = 0
+				return nil
 			} else {
 				if stop := (stopError{}); errors.As(err, &stop) {
 					// Since the 'stop' command halts execution of the entire script,

script/scripttest/scripttest_test.go
@@ -46,6 +46,7 @@ func TestAll(t *testing.T) {
 				},
 			},
 			func(s *script.State, args ...string) (script.WaitFunc, error) {
+				s.Logf("retrytest called")
 				notEmpty, err := s.Flags.GetString("not-empty")
 				if err != nil {
 					return nil, err

commit be12451887fc40307aae0cf10f1d26d46cd6689c

script: After retry we should not stop executing further commands
This was accidentally introduced in previous commit.

Fixes: 08596a874b92 ("script: Fix lack of newline on retried command in logs")
Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

script/engine.go
@@ -372,7 +372,6 @@ func (e *Engine) Execute(s *State, file string, script *bufio.Reader, log io.Wri
 				}
 				fmt.Fprintf(log, "(command %q succeeded after %d retries in %.3fs)\n", line, s.RetryCount, time.Since(retryStart).Seconds())
 				s.RetryCount = 0
-				return nil
 			} else {
 				if stop := (stopError{}); errors.As(err, &stop) {
 					// Since the 'stop' command halts execution of the entire script,